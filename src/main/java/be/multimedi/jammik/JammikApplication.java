ba4a021988644416991d1d422a04f88b26601bb7
Jan Olaerts <jan.olaerts92@gmail.com>
2020-10-16 11:25:29 +0200

    initial commit

---
+++ src/main/java/be/multimedi/jammik/JammikApplication.java
@@ -0,0 +1,13 @@
+package be.multimedi.jammik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JammikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JammikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/multimedi/jammik/JammikApplicationTests.java
@@ -0,0 +1,13 @@
+package be.multimedi.jammik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JammikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
