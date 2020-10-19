a75710b832aed6e0a22d1b342164a0e6d1a181f0
michael creelle <michael.creelle@telenet.be>
2020-10-19 10:54:27 +0200

    created and finnished Menu

---
+++ src/main/java/be/multimedi/jammik/entities/Menu.java
@@ -0,0 +1,40 @@
+package be.multimedi.jammik.entities;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.List;
+
+// Created by: Michael Creelle
+
+@Entity
+@Table(name = "Menu")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @OneToMany
+    @Column(name = "MenuItems")
+    private List<MenuItem> menuItems;
+
+    public void setId(int id) {
+        if (id < 1) throw new IllegalArgumentException("id mag niet 0 of negatief zijn");
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        if (menuItems == null || menuItems.size() < 1) throw new IllegalArgumentException("MenuItems mag niet null of leeg zijn");
+        this.menuItems = menuItems;
+    }
+}
