f927f3d7db02265e1055e36c587ecbcccabb3aa1
Jan Olaerts <jan.olaerts92@gmail.com>
2020-10-19 14:23:21 +0200

    OpeningsUren entity done

---
+++ src/main/java/be/multimedi/jammik/entities/OpeningsUren.java
@@ -0,0 +1,44 @@
+package be.multimedi.jammik.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+
+/**
+ * Gemaakt door Jan
+ */
+@Entity
+@Table(name="openingsuren")
+public class OpeningsUren {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Min(1)
+    private int id;
+
+    @OneToMany(mappedBy="openingsUur", fetch=FetchType.EAGER, cascade=CascadeType.REMOVE)
+    private List<Dag> dagen;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Dag> getDagen() {
+        return dagen;
+    }
+
+    public void setDagen(List<Dag> dagen) {
+        this.dagen = dagen;
+    }
+
+    @Override
+    public String toString() {
+        return "OpeningsUren{" +
+                "id=" + id +
+                ", dagen=" + dagen +
+                '}';
+    }
+}
