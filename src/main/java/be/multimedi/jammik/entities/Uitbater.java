cd28b571eb668eee10e943c353dd529f8e54efa8
michael creelle <michael.creelle@telenet.be>
2020-10-19 13:30:22 +0200

    created and finnished Uitbater

---
+++ src/main/java/be/multimedi/jammik/entities/Uitbater.java
@@ -0,0 +1,28 @@
+package be.multimedi.jammik.entities;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+// Gemaakt door: Michael Creelle
+
+@Entity
+@Table(name = "Uitbater")
+public class Uitbater extends Person{
+    @NotNull
+    @OneToMany
+    @Column(name = "Zaken")
+    private List<Zaak> zaken;
+
+    public List<Zaak> getZaken() {
+        return zaken;
+    }
+
+    public void setZaken(List<Zaak> zaken) {
+        if (zaken == null) throw new IllegalArgumentException("zaken mag niet null zijn");
+        this.zaken = zaken;
+    }
+}
