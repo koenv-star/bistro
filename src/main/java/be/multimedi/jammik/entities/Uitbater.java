9517c0f75e87e7fd6eabe48248efd086fe646876
michael creelle <michael.creelle@telenet.be>
2020-10-19 14:11:53 +0200

    created and finnished zaak

--- src/main/java/be/multimedi/jammik/entities/Uitbater.java
+++ src/main/java/be/multimedi/jammik/entities/Uitbater.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 // Gemaakt door: Michael Creelle
 
