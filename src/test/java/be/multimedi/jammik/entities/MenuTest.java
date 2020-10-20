0d36352bd4a4b2634f614b789581b27e28be4a73
michael creelle <michael.creelle@telenet.be>
2020-10-20 09:34:26 +0200

    finnished MenuTest

--- src/test/java/be/multimedi/jammik/entities/MenuTest.java
+++ src/test/java/be/multimedi/jammik/entities/MenuTest.java
@@ -26,10 +26,30 @@ class MenuTest {
     }
 
     @Test
+    void throw_exception_on_negative_id(){
+        assertThrows(IllegalArgumentException.class, () -> menu.setId(-1));
+    }
+
+    @Test
+    void throw_exception_on_0_id() {
+        assertThrows(IllegalArgumentException.class, () -> menu.setId(0));
+    }
+
+    @Test
     void set_get_MenuItems() {
         List<MenuItem> menuItems = new ArrayList<>();
         menuItems.add(new MenuItem());
         menu.setMenuItems(menuItems);
         assertEquals(menuItems, menu.getMenuItems());
+    }
+
+    @Test
+    void throws_exception_on_null_menuItems() {
+        assertThrows(IllegalArgumentException.class, () -> menu.setMenuItems(null));
+    }
+
+    @Test
+    void throws_exception_on_empty_list_menuItems() {
+        assertThrows(IllegalArgumentException.class, () -> menu.setMenuItems(new ArrayList<>()));
     }
 }
