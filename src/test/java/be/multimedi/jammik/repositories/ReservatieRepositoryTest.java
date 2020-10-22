1a3f3122999bb9b9fa4ef523af4a9292c62ed8f2
Jan Olaerts <jan.olaerts92@gmail.com>
2020-10-22 15:43:19 +0200

    ReservatieRepositoryTest written

---
+++ src/test/java/be/multimedi/jammik/repositories/ReservatieRepositoryTest.java
@@ -0,0 +1,66 @@
+package be.multimedi.jammik.repositories;
+
+import be.multimedi.jammik.entities.Reservatie;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Gemaakt door Jan
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class ReservatieRepositoryTest {
+
+    private ReservatieRepository rr;
+    private KlantRepository kr;
+    private ZaakRepository zr;
+    private TafelRepository tr;
+
+    @Autowired
+    public ReservatieRepositoryTest(ReservatieRepository rr, KlantRepository kr, ZaakRepository zr, TafelRepository tr) {
+        this.rr = rr;
+        this.kr = kr;
+        this.zr = zr;
+        this.tr = tr;
+    }
+
+    @Test
+    void getById() {
+        Reservatie reservatie = rr.getReservatieById(3);
+        assertNotNull(reservatie);
+        assertEquals("jammik@bistro.be", reservatie.getKlant().getEmail());
+    }
+
+    @Test
+    void findAll() {
+        List<Reservatie> reservaties = rr.findAll();
+        assertNotNull(reservaties);
+        assertEquals(3, reservaties.size());
+    }
+
+    @Test
+    void save() {
+        Reservatie reservatie = new Reservatie(0, LocalDateTime.of(2021, 12, 25, 9, 15),
+                LocalTime.of(1, 0), 125.5, kr.getKlantByEmail("jammik@bistro.be"), zr.getZaakById(2), tr.getTafelById(1));
+
+        reservatie = rr.save(reservatie);
+        assertEquals(4, reservatie.getId());
+    }
+
+    @Test
+    void deleteById() {
+        rr.deleteById(2);
+        assertEquals(2, rr.findAll().size());
+    }
+}
