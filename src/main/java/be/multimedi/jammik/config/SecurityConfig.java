129cd27258cdbf7ae9df1ac3a267fef76acc1fbb
Mehmet Durmus <5480444-MhtZy@users.noreply.gitlab.com>
2020-10-28 09:29:06 +0100

    last changes

--- src/main/java/be/multimedi/jammik/config/SecurityConfig.java
+++ src/main/java/be/multimedi/jammik/config/SecurityConfig.java
@@ -72,8 +72,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService);
-
-
     }
 
 
